d923cfc8f114b0b57b6cc3a730f382c1d689fd7d
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-04-13 14:43:06 +0530

    Completed TALib

--- tal.py
+++ tal.py
@@ -1,5 +1,28 @@
 import numpy
 import talib
+from numpy import genfromtxt
 
 close=numpy.random.random(100)
-print(close)
+# print(close)
+
+
+# moving average
+moving_average=talib.SMA(close,timeperiod=10)
+#print(moving_average)
+
+# rsi indicator
+rsi=talib.RSI(close)
+#print(rsi)
+
+
+# reading data from csv file
+my_data=genfromtxt('candlestick.csv',delimiter=',')
+#print(my_data)
+
+# reading a specific field
+closePrice=my_data[:,4]
+#print(closePrice)
+
+# rsi of close price
+rsi=talib.RSI(close)
+print(rsi)
