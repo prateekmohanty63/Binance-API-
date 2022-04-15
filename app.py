6c7293290e6a9e289a586cf4c5230395956f8be8
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-04-15 23:40:23 +0530

    Completed buy endpoint

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
-from flask import Flask, render_template
+from crypt import methods
+from django.shortcuts import render,redirect
+from flask import Flask, render_template,request,flash
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+# from requests import request
 import config
 import csv
 from binance.enums import *
 
 
 app=Flask(__name__)
+app.secret_key=config.SECRET_KEY
 
 client = Client(config.API_KEY,config.API_SECRET)
 
@@ -28,16 +31,24 @@ def index():
 
     return render_template('index.html',title=title)
 
-@app.route('/buy')
+@app.route('/buy',methods=['GET','POST'])
 def buy():
-    order = client.create_order(
-    symbol='BNBBTC',
-    side=SIDE_BUY,
-    type=ORDER_TYPE_LIMIT,
-    timeInForce=TIME_IN_FORCE_GTC,
-    quantity=100,
-    price='0.00001')
-    return 'buy'
+
+    symbol=request.form['symbol']
+    quantity=request.form['quantity']
+    try:
+        order = client.create_order(
+        symbol=symbol,
+        side=SIDE_BUY,
+        type=ORDER_TYPE_LIMIT,
+        quantity=quantity,
+        price='0.00001')
+    
+    except Exception as e:
+        flash(e.message,"error")
+
+    
+    return redirect('/')
 
 @app.route('/sell')
 def sell():
